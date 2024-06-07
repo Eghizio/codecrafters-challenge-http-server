350ef095f67432d86cfa4b9a1786ab01953331da
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-06-07 02:14:57 +0200

    Parse request, route based on url to OK or NOT_FOUND

--- app/main.ts
+++ app/main.ts
@@ -1,8 +1,29 @@
 import * as net from "net";
 
+const HTTP_STATUS = {
+  OK: `HTTP/1.1 200 OK\r\n\r\n`,
+  NOT_FOUND: `HTTP/1.1 404 Not Found\r\n\r\n`,
+};
+
+const parseRequestData = (data: Buffer) => {
+  const requestLine = data.toString().split("\r\n")[0];
+
+  const [httpMethod, requestTarget, httpVersion] = requestLine.split(" ");
+  console.log({ httpMethod, requestTarget, httpVersion });
+
+  return { httpMethod, requestTarget, httpVersion };
+};
+
 const server = net.createServer((socket) => {
-  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-  socket.end();
+  socket.on("data", (data) => {
+    const { requestTarget } = parseRequestData(data);
+
+    const response =
+      requestTarget === "/" ? HTTP_STATUS.OK : HTTP_STATUS.NOT_FOUND;
+
+    socket.write(Buffer.from(response));
+    socket.end();
+  });
 });
 
 console.log("Logs from your program will appear here!");
