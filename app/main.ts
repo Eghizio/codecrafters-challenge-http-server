58d16520d6756d1d036e3908158e3dd98ff35cef
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2024-06-07 02:57:39 +0200

    Parse User-Agent header and return as response

--- app/main.ts
+++ app/main.ts
@@ -35,20 +35,19 @@ const parseSerializedData = (serialized: string) => {
 };
 
 const parseData = (data: Buffer) => {
-  //   return parseSerializedData(data.toString());
-  const serialized = data.toString();
-  const { request, headers, body } = parseSerializedData(serialized);
-
-  console.log(serialized, { request, headers, body });
-
-  return { request, headers, body };
+  return parseSerializedData(data.toString());
 };
 
+type Incoming = ReturnType<typeof parseData>;
+
 const buildResponse = (status: string, headers: string, body: any): string => {
+  if (!headers && !body) return `${status}${HEADERS_END}`;
+  if (!headers) return `${status}${HEADERS_END}${body}`;
+
   return `${status}${LINE_END}${headers}${HEADERS_END}${body}`;
 };
 
-const createResponse = (requestTarget: string) => {
+const createResponse = ({ request: { requestTarget }, headers }: Incoming) => {
   if (requestTarget.startsWith("/echo/")) {
     const body = requestTarget.replace("/echo/", "");
     const contentLength = body.length;
@@ -61,21 +60,31 @@ const createResponse = (requestTarget: string) => {
   }
 
   switch (requestTarget) {
-    case "/":
+    case "/": {
       return buildResponse(HTTP_STATUS.OK, "", "");
-    default:
+    }
+
+    case "/user-agent": {
+      const userAgent = headers["User-Agent"];
+      return buildResponse(
+        HTTP_STATUS.OK,
+        `Content-Type: text/plain\r\nContent-Length: ${userAgent.length}`,
+        userAgent
+      );
+    }
+
+    default: {
       return buildResponse(HTTP_STATUS.NOT_FOUND, "", "");
+    }
   }
 };
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    const {
-      request: { requestTarget },
-    } = parseData(data);
+    const incoming = parseData(data);
+    const response = createResponse(incoming);
 
-    const response = createResponse(requestTarget);
-    console.log({ response });
+    console.log({ incoming, response });
 
     socket.write(Buffer.from(response));
     socket.end();
